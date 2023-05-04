952ae402561417483b978402476e6ea4974c28fe
Aayush Basnet <122598100+Aayush-Basnet@users.noreply.github.com>
2023-05-04 18:26:39 +0545

    Arthimetic operation on Python 01
    First program #1

---
+++ 01.py
@@ -0,0 +1,14 @@
+ 
+a = 10
+b = 20
+
+sum = a+b
+diff = a-b
+mul = a*b
+div = a/b 
+mod = a% b
+print(f"The sum is {sum}")
+print(f"The difference is {diff}")
+print(f"The Multiplication is {mul}")
+print(f"The Division is {div}")
+print(f"The Modulus is {mod}")
