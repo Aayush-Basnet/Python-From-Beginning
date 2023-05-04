daa64589828a7539ee89fec26e4672af384a0979
Aayush Basnet <122598100+Aayush-Basnet@users.noreply.github.com>
2023-05-04 18:27:59 +0545

    Arithmetic operation #2
    How to take input from user in python while doing mathmatical operation?

---
+++ 02.py
@@ -0,0 +1,14 @@
+
+a = int( input("Enter first number: "))
+b =int( input("Enter second number: "))
+
+sum = a+b
+diff = a-b
+mul = a*b
+div = a/b 
+mod = a% b
+print(f"The sum is {sum}")
+print(f"The difference is {diff}")
+print(f"The Multiplication is {mul}")
+print(f"The Division is {div}")
+print(f"The Modulus is {mod}")
