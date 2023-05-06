ad1f72f452871e7c32b80e663b42f0b43e778519
Aayush Basnet <122598100+Aayush-Basnet@users.noreply.github.com>
2023-05-06 20:47:49 +0545

    Numeric Data Type
    - int
    - float
    - complex

---
+++ 04.py
@@ -0,0 +1,18 @@
+
+a = 2.4
+b = 4
+c= 3+2j
+d= 2+1j
+e= True
+sum = a+b
+add = c+d
+
+print(sum)
+print(add)
+print(e)
+print(type(a))
+print(type(b))
+print(type(c))
+
+name= input("Enter your name: ")
+print (f"Hello{name}")
