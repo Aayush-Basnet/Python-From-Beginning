a25df8c9d0199b34cad2ce004bff432fccb32743
Aayush Basnet <122598100+Aayush-Basnet@users.noreply.github.com>
2023-05-06 20:46:13 +0545

    playing with String variable

---
+++ 03.py
@@ -0,0 +1,15 @@
+
+a="Aayush"
+name= input("Enter your name: ")
+print (f"Hello {name}")
+print(a)
+print(a[0])
+print (a[0:4])
+print(a[:4])
+print(a[0:])
+print(a[1:])
+print(a[-0])
+print(a[-1])
+print(a[-2])
+print(a[-3])
+print(a[-4])
