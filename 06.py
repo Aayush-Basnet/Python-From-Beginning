91caf94a795277213e77c1872f926cda0798ed40
Aayush Basnet <122598100+Aayush-Basnet@users.noreply.github.com>
2023-05-08 21:19:26 +0545

    List and tuple

---
+++ 06.py
@@ -0,0 +1,11 @@
+#Lists
+days=['sunday','monday',20,'Tuesday','wednesday']
+print(days)
+days[0]='sunny day' # can change index value in list
+print(days[0])
+
+#tuple
+tp=(1,2,3,4,5,6)
+print(tp)
+#tp[1]=9 cannot change index value in tuple
+print(tp[1]) 
