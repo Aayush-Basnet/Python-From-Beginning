87243bb88abdbb129bc89a9f2ce467fab1466af0
Aayush Basnet <122598100+Aayush-Basnet@users.noreply.github.com>
2023-05-15 16:51:13 +0545

    Arithmetic Operation
    Sum, Diff, Mul, Div and Mod of Data

---
+++ arithmeticoperation.py
@@ -0,0 +1,13 @@
+a=10
+b = 4
+add=a+b
+sub=a-b
+mul=a*b
+div= a/b
+mod = a%b
+
+print(f"The sum is {add}")
+print(f"The difference is {sub}")
+print(f"The Multiplication is {mul}")
+print(f"The Division is {div}")
+print(f"The modulus is {mod}")
---
+++ arithmeticoperation2.py
@@ -0,0 +1,13 @@
+a=int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+add=a+b
+sub=a-b
+mul=a*b
+div= a/b
+mod = a%b
+
+print(f"The sum is {add}")
+print(f"The difference is {sub}")
+print(f"The Multiplication is {mul}")
+print(f"The Division is {div}")
+print(f"The modulus is {mod}")
