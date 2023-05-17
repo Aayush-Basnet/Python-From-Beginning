f1720c37a7c9695b7b36f77d0d3d8b40a5b3ac3d
Aayush Basnet <122598100+Aayush-Basnet@users.noreply.github.com>
2023-05-17 19:36:55 +0545

    swapping of variable

---
+++ swapping.py
@@ -0,0 +1,10 @@
+a=int(input("Enter value of a: "))
+b=int(input("Enter value of b: "))
+c= int
+print(f"Before swapping Value of a is: {a}")
+print(f"Before swapping Value of b is: {b}")
+c=b
+b=a
+a=c
+print(f"After swapping Value of a is: {a}")
+print(f"After swapping Value of b is: {b}")
