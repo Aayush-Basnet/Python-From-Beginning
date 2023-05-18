7946aea1708010df6231d1441f7d9c42566862c7
Aayush Basnet <122598100+Aayush-Basnet@users.noreply.github.com>
2023-05-18 20:42:09 +0545

    Is that a Vowel?

---
+++ VowelorNot.py
@@ -0,0 +1,12 @@
+
+letter = input("Enter letter: ")
+
+lowercase= int
+uppercase = int
+lowercase=(letter=='a'or letter=='i'or letter=='o'or letter=='u'or letter=='e')
+uppercase=(letter=='A'or letter=='E'or letter=='I'or letter=='O'or letter=='U')
+
+if(lowercase or uppercase):
+    print(f"{letter} is vowel")
+else:
+    print(f"{letter} is not vowel")
