bc63c2aaf5297f8e4fbbb391ffe1fffeb3901b50
Aayush Basnet <122598100+Aayush-Basnet@users.noreply.github.com>
2023-05-19 21:10:36 +0545

    Swapping the value using two variables
    Formula:
    a=a+b
    b=a-b
    a=a-b

---
+++ swappingusingonlytwovariable.py
@@ -0,0 +1,7 @@
+a=int(input("Enter value of a: "))
+b= int(input("Enter value of b: "))
+a=a+b
+b=a-b
+a=a-b
+print("After swapping the value of a is ",a)
+print("After swapping the value of b is ",b)
