12a6dd67d14d0cf02bc8978cc1cc7b6e4936b830
Aayush Basnet <122598100+Aayush-Basnet@users.noreply.github.com>
2023-05-20 19:15:46 +0545

    Various way of using for loop

---
+++ forloop.py
@@ -0,0 +1,10 @@
+days= ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+
+for i in days:
+    print(i)
+    
+for j in range(10):
+    print(j,"Aayush")
+
+for i in range(20,30,2):
+    print(i)
