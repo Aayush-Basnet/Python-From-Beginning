a1c7fec6a6f783c38518933e081371f8e22eec0f
Aayush Basnet <122598100+Aayush-Basnet@users.noreply.github.com>
2023-05-20 19:14:25 +0545

    Conversion of Numbers
    Number System..
    Binary to decimal,octal and hexadecimal.
    Decimal to binary,octal and hexadecimal.
    octal to binary, decimal and hexadecimal.
    Hexadecimal to binary, decimal and octal..

---
+++ numberconversion.py
@@ -0,0 +1,20 @@
+a=14        # decimal number
+print(bin(a))       # binary conversion
+print(oct(a))       # octal conversion
+print(hex(a))       # hexadecimal conversion
+
+b=0x3F      #hexadecimal number
+print(b)    #decimal conversion
+print(bin(b))   #binary conversion
+print(oct(b))   #octal conversion
+
+
+c=0b1101        #binary number
+print(c)        #decimal conversion
+print(oct(c))   #octal conversion
+print(hex(c))   #hexadecimal conversion
+
+d= 0o45         #octal number
+print(d)        #decimal conversion
+print(bin(d))   #binary conversion
+print(hex(d))   #hexadecimal conversion
